ae44e8b53cd75e9966985f001d856cffc28b9838
sb7301 <sb7301@srmist.edu.in>
2026-03-10 15:23:32 +0530

    sb7301-dot UC6: Queue + Stack Based Palindrome Check

--- app/src/PalindromeApp.java
+++ app/src/PalindromeApp.java
@@ -1,29 +1,6 @@
-import java.util.Scanner;
+import java.util.Stack;
 public class PalindromeApp {
     public static void main(String[] args) {
-        String input = "radar";
 
-
-        char[] chars = input.toCharArray();
-
-
-        int start = 0;
-
-        int end = chars.length - 1;
-
-        boolean isPalindrome = true;
-
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
-        }
-
-
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
