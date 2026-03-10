fa010c36bc67d836d1082209f3184ddd3c992488
sb7301 <sb7301@srmist.edu.in>
2026-03-10 14:35:30 +0530

    UC4: Character Array Based Palindrome Check

--- app/src/PalindromeApp.java
+++ app/src/PalindromeApp.java
@@ -1,19 +1,6 @@
 import java.util.Scanner;
 public class PalindromeApp {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a word: ");
-        String input = scanner.nextLine();
-        input = input.replaceAll("\\s+", "").toLowerCase();
-        String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
-        }
-        if (input.equals(reversed)) {
-            System.out.println("The input is a palindrome.");
-        } else {
-            System.out.println("The input is NOT a palindrome.");
-        }
-        scanner.close();
+
     }
 }
